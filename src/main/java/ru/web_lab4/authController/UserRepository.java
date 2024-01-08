b9a2bcb578ceec10a900b6586650fafd98c1364e
Azat222 <azat222@gmail.com>
2024-01-08 16:00:02 +0500

    feat: ended up with oauth2.0 implementation

--- src/main/java/ru/web_lab4/authController/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.web_lab4.authController;
-
-import lombok.NonNull;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
-    ApplicationUser findApplicationUserByLogin(@NonNull String login);
-}
--- src/main/java/ru/web_lab4/dotsController/DotsController.java
+++ src/main/java/ru/web_lab4/dotsController/DotsController.java
@@ -3,10 +3,10 @@ package ru.web_lab4.dotsController;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import ru.web_lab4.authController.AuthService;
 
 import java.util.List;
 
@@ -16,33 +16,29 @@ public class DotsController {
     @Autowired
     private final DotsRepository dotsRepository;
 
-    @Autowired
-    private final AuthService authService;
 
     @PostMapping("/api/dots")
     public Dot addDot(@RequestParam("x") Float x,
                       @RequestParam("y") Float y,
                       @RequestParam("r") Float r,
-                      @RequestHeader("Authorization") String authorization){
+                      Authentication authentication){
         long timer = System.nanoTime();
-        String login = authService.checkUser(authorization);
         if (!Dot.validateInput(x, y, r))
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Not valid x, y, r values");
-        Dot dot = new Dot(x, y, r, login);
+        Dot dot = new Dot(x, y, r, authentication.getName());
         dot.setScriptTime(System.nanoTime() - timer);
         dotsRepository.save(dot);
         return dot;
     }
 
     @GetMapping("/api/dots")
-    public List<Dot> getDots(@RequestHeader("Authorization") String authorization){
-        return dotsRepository.getDotByOwnerLogin(authService.checkUser(authorization));
+    public List<Dot> getDots(Authentication authentication){
+        return dotsRepository.getDotByOwnerLogin(authentication.getName());
     }
 
     @Transactional
     @DeleteMapping("/api/dots")
-    public void deleteDots(@RequestHeader("Authorization") String authorization){
-        String login = authService.checkUser(authorization);
-        dotsRepository.deleteDotsByOwnerLogin(login);
+    public void deleteDots(Authentication authentication){
+        dotsRepository.deleteDotsByOwnerLogin(authentication.getName());
     }
 }
